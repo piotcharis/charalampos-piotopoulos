691e4ffdb6d3af63fae65ba58c4c893cb02ab684
Charis <piotcharis@gmail.com>
2023-04-16 21:05:19 +0200

    added python

--- src/Components/Sections/Education.js
+++ src/Components/Sections/Education.js
@@ -1,34 +1,48 @@
 import React, { Fragment } from "react";
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 import { Timeline, Event } from "react-timeline-scribble";
-import "../../App.css"
+import "../../App.css";
 
-function Education () {
-    return (
-        <div className="divEdu">
-            <h1 className="title">Education</h1>
-            <Fragment>
-                <Timeline>
-                        <Fade delay={50} bottom={true}>
-                            <Event interval={"2021 - present"} title={"Technical University of Munich (TUM)"} subtitle={"Computer Science (Bachelor of Science)"}>
-                                Application Subject: Medicine.
-                            </Event>
-                        </Fade>
-                        <Fade delay={100} bottom={true}>
-                        <Event interval={"2018 - 2021"} title={"University of Saarland"} subtitle={"Medicine (Staatsexamen)"}>
-                            Successfully completed courses: Chemistry, Clinical Chemistry, Biology, Clinical Biology, Physics, Anatomy, Biochemistry, 
-                            Histology, Physiology, Clinical Physiology, Medical Terminology, Psychology, Sociology and Clinical Medicine.
-                        </Event>
-                        </Fade>
-                        <Fade delay={150} bottom={true}>
-                        <Event interval={"2012 - 2018"} title={"German School of Athens (DSA)"} subtitle={"Allgemeine Hochschulreife (Abitur)"}>
-                            Grade: 1.2
-                        </Event>
-                        </Fade>
-                    </Timeline>
-            </Fragment>
-        </div>
-    );
+function Education() {
+  return (
+    <div className="divEdu">
+      <h1 className="title">Education</h1>
+      <Fragment>
+        <Timeline>
+          <Fade delay={50} bottom={true}>
+            <Event
+              interval={"2021 - present"}
+              title={"Technical University of Munich (TUM)"}
+              subtitle={"Computer Science (Bachelor of Science)"}
+            >
+              Application Subject: Medicine
+            </Event>
+          </Fade>
+          <Fade delay={100} bottom={true}>
+            <Event
+              interval={"2018 - 2021"}
+              title={"University of Saarland"}
+              subtitle={"Medicine (Staatsexamen)"}
+            >
+              Successfully completed courses: Chemistry, Clinical Chemistry,
+              Biology, Clinical Biology, Physics, Anatomy, Biochemistry,
+              Histology, Physiology, Clinical Physiology, Medical Terminology,
+              Psychology, Sociology and Clinical Medicine.
+            </Event>
+          </Fade>
+          <Fade delay={150} bottom={true}>
+            <Event
+              interval={"2012 - 2018"}
+              title={"German School of Athens (DSA)"}
+              subtitle={"Allgemeine Hochschulreife (Abitur)"}
+            >
+              Grade: 1.2
+            </Event>
+          </Fade>
+        </Timeline>
+      </Fragment>
+    </div>
+  );
 }
 
 export default Education;
